162a158bdd942d44d8d6a3b73dc78217d686a409
Tyrel Hiebert <tyrelh@uvic.ca>
2019-03-31 08:38:11 -0700

    decrease duration of scroll animation

--- src/components/ScrollToId.jsx
+++ src/components/ScrollToId.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import scrollToElement from "scroll-to-element";
-import { generateShowHourMinuteSecond } from "antd/lib/time-picker";
-import renderEmpty from "antd/lib/config-provider/renderEmpty";
 
 class ScrollToId extends React.Component {
 
@@ -11,7 +9,7 @@ class ScrollToId extends React.Component {
     offset: -20,
     align: "top",
     ease: "in-out-expo",
-    duration: 800
+    duration: 700
   });
 
   render () {
