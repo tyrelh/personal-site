3f55f4e16b8d8c517559f100abb00c6c607041f4
Tyrel Hiebert <tyrelh@uvic.ca>
2019-11-27 06:54:49 -0800

    refactor out sticky header for possible use later

---
+++ src/components/StickyHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class StickyHeader extends React.Component {
+  render (
+    {/* <div id="sticky-header" className="sticky-header-hidden">
+          <NavLink exact to={this.state.link}>
+            <h1 datatext={this.state.shortTitle}>
+              {this.state.shortTitle}
+            </h1>
+          </NavLink>
+        </div> */}
+  )
+}
