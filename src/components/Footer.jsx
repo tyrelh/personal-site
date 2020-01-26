e5fe15a1fc24b5f754ca2315b79daa55fab9e56e
tyrelh <krankyjoe@gmail.com>
2020-01-26 14:42:06 -0800

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./footer.scss";
+import {Icon} from "antd";
 
 export default class Footer extends React.Component {
   render() {
     return (
       <footer>
+        <p>
+          Made with <Icon type="heart" theme="filled" /> by Tyrel Hiebert
+        </p>
       </footer>
     )
   }
