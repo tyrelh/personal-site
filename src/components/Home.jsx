97e073c4598336850cf35bcff70c52189a2c80cd
tyrelh <krankyjoe@gmail.com>
2020-01-30 22:26:36 -0800

    remove section styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import "./section.scss"
 import SectionHeader from "./elements/SectionHeader";
 import ArticlesOverview from "./elements/ArticlesOverview";
 
