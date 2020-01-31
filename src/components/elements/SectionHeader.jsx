8576db1c9ebe61c6d884b9c17055d38d9c22b066
tyrelh <krankyjoe@gmail.com>
2020-01-31 08:57:05 -0800

    first cut of a link icon for the section headers

--- src/components/elements/SectionHeader.jsx
+++ src/components/elements/SectionHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SectionHeader.scss";
+import {Icon} from "antd";
 
 export default class SectionHeader extends React.Component {
   render() {
     return (
       <h2>
+        {/*<Icon className="heading-link-icon" type="link" />*/}
         <span className="underline" id={this.props.id}>
           {this.props.children}
         </span>
