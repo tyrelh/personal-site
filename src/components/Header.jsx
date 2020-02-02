dca3ec5a77445e97c6a9062e13338083854c1993
tyrelh <krankyjoe@gmail.com>
2020-02-01 19:31:55 -0800

    underline effect on header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,13 +12,15 @@ export default class Header extends React.Component {
         <div className="nameContainer fadeIn">
             <NavLink exact to="/">
               <h1 datatext={this.props.headerTitle} className={this.classN}>
-                {this.props.headerTitle}
-                {
-                  (this.props.children) ?
-                    this.props.children
-                    :
-                    null
-                }
+                <span className="underline">
+                  {this.props.headerTitle}
+                  {
+                    (this.props.children) ?
+                      this.props.children
+                      :
+                      null
+                  }
+                </span>
               </h1>
             </NavLink>
         </div>
