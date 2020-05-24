e75416aa3014cf3f213f5da6efa3c5ff6bd53533
tyrelh <krankyjoe@gmail.com>
2020-05-23 23:41:53 -0700

    remove header underline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,15 +12,13 @@ export default class Header extends React.Component {
         <div className="nameContainer fadeIn">
             <NavLink exact to="/">
               <h1 datatext={this.props.headerTitle} className={this.classN}>
-                <span className="underline">
-                  {this.props.headerTitle}
-                  {
-                    (this.props.children) ?
-                      this.props.children
-                      :
-                      null
-                  }
-                </span>
+                {this.props.headerTitle}
+                {
+                  (this.props.children) ?
+                    this.props.children
+                    :
+                    null
+                }
               </h1>
             </NavLink>
         </div>
