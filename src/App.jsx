ba3fe35aa33de854fbdbc2893615385492100f98
tyrelh <krankyjoe@gmail.com>
2020-05-30 20:28:08 -0700

    refactor out blog piece from article paths

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ import "./app.scss";
 import DarkModeToggle from "./components/articles/DarkModeToggle";
 import LightshiftGame from "./components/articles/LightshiftGame";
 import DenoAndGithubActions from "./components/articles/DenoAndGithubActions";
-import BackHome from "./components/elements/BackHome";
+// import BackHome from "./components/elements/BackHome";
 import NodeCICDPipeline from "./components/articles/NodeCICDPipeline";
 
+const blogPerma = "/blog";
 
 export default class App extends React.Component {
-
   render() {
     return (
       <div className="app">
@@ -24,13 +24,13 @@ export default class App extends React.Component {
         {/*<BackHome/>*/}
         <BackToTop/>
         <Route path="/" component={Home} exact />
-        <Route path="/blog/2020/deno-tests-and-github-actions" component={DenoAndGithubActions}/>
-        <Route path="/blog/2020/lightshift-game" component={LightshiftGame}/>
-        <Route path="/blog/2020/dark-mode-react-toggle" component={DarkModeToggle}/>
-        <Route path="/blog/2019/grails-frontend-asset-migration" component={GrailsAssetMigration}/>
-        <Route path="/blog/2018/battlesnake-2018" component={Battlesnake2018}/>
-        <Route path="/blog/2017/mechanical-keyboard" component={MechanicalKeyboard}/>
         <Route path={`${blogPerma}/2020/node-cicd-pipeline-using-github-actions-and-aws-elastic-beanstalk`} component={NodeCICDPipeline}/>
+        <Route path={`${blogPerma}/2020/deno-tests-and-github-actions`} component={DenoAndGithubActions}/>
+        <Route path={`${blogPerma}/2020/lightshift-game`} component={LightshiftGame}/>
+        <Route path={`${blogPerma}/2020/dark-mode-react-toggle`} component={DarkModeToggle}/>
+        <Route path={`${blogPerma}/2019/grails-frontend-asset-migration`} component={GrailsAssetMigration}/>
+        <Route path={`${blogPerma}/2018/battlesnake-2018`} component={Battlesnake2018}/>
+        <Route path={`${blogPerma}/2017/mechanical-keyboard`} component={MechanicalKeyboard}/>
         {/*<Route component={Home} />*/}
         <Footer />
       </div>
