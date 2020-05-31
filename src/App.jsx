a833cd99d9ceb74df15c7ae4662b9d37f6ac4030
tyrelh <krankyjoe@gmail.com>
2020-05-30 20:26:37 -0700

    refactor out blog piece from url

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import DarkModeToggle from "./components/articles/DarkModeToggle";
 import LightshiftGame from "./components/articles/LightshiftGame";
 import DenoAndGithubActions from "./components/articles/DenoAndGithubActions";
 import BackHome from "./components/elements/BackHome";
+import NodeCICDPipeline from "./components/articles/NodeCICDPipeline";
 
 
 export default class App extends React.Component {
@@ -29,6 +30,7 @@ export default class App extends React.Component {
         <Route path="/blog/2019/grails-frontend-asset-migration" component={GrailsAssetMigration}/>
         <Route path="/blog/2018/battlesnake-2018" component={Battlesnake2018}/>
         <Route path="/blog/2017/mechanical-keyboard" component={MechanicalKeyboard}/>
+        <Route path={`${blogPerma}/2020/node-cicd-pipeline-using-github-actions-and-aws-elastic-beanstalk`} component={NodeCICDPipeline}/>
         {/*<Route component={Home} />*/}
         <Footer />
       </div>
