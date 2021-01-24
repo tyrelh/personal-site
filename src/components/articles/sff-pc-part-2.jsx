42159eb7b11f3ed96a285e9ac289fdd48390e6ff
tyrelh <krankyjoe@gmail.com>
2021-01-24 14:31:20 -0800

    create sff pc part 2 article component

---
+++ src/components/articles/sff-pc-part-2.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MarkdownArticle from "../MarkdownArticle";
+
+export default class SFFPCPart2 extends React.Component {
+  render() {
+    return <MarkdownArticle name="sff-pc-part-2"/>;
+  }
+}
