b9373c4264443bd397c528e84e6530c2713cffd5
tyrelh <krankyjoe@gmail.com>
2021-01-26 20:39:10 -0800

    home button will now scroll back to the top of the page

--- src/components/elements/StickyHeader.jsx
+++ src/components/elements/StickyHeader.jsx
@@ -1,26 +1,44 @@
 import React from "react";
 import "./StickyHeader.scss";
-import {Icon} from "antd";
-import {NavHashLink as NavLink} from "react-router-hash-link";
+import {Icon, Tooltip} from "antd";
+import { Redirect } from "react-router-dom";
 
 export default class StickyHeader extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      redirect: false,
+      redirectLink: ""
+    }
+  }
+
+  handleOnClick = (link) => {
+    this.setState({
+      redirect: true,
+      redirectLink: link
+    }, () => window.scrollTo(0, 0));
+  };
+
   render () {
+    if (this.state.redirect) {
+      return <Redirect push to={this.state.redirectLink} />;
+    }
+
     return (
-      // <Tooltip
-      //   title="Home"
-      //   placement="bottomLeft"
-      //   arrowPointAtCenter
-      //   mouseEnterDelay={0.2}
-      // >
-        <div id="sticky-header" className="sticky-header-hidden">
-          <NavLink to={this.props.link}>
-              {/* <h1 datatext={this.props.text}>
-                {this.props.text}
-              </h1> */}
-              <Icon type="home" theme="filled"/>
-            </NavLink>
-        </div>
-      // </Tooltip>
+      <div
+        id="sticky-header"
+        className="sticky-header-hidden"
+        onClick={() => this.handleOnClick(this.props.link)}
+      >
+        <Tooltip
+          title="Home"
+          placement="bottomLeft"
+          arrowPointAtCenter
+          mouseEnterDelay={0.2}
+        >
+          <Icon type="home" theme="filled"/>
+        </Tooltip>
+      </div>
     )
   }
 }
