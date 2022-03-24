e16839da3be8b5156354164206028e446057faea
tyrelh <krankyjoe@gmail.com>
2022-03-23 20:55:25 -0700

    remove plausible component for now

---
+++ src/tools/Plausible.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { render } from "react-dom";
+
+// class Plausible extends React.Component {
+//     env = "%NODE_ENV%";
+//     if (env === "production") {
+//         render() {
+//             return (
+//                 <>
+//                     <script defer data-domain="tyrelh.github.io" src="https://plausible.io/js/plausible.js"></script>
+//                     { props.children }
+//                 </>
+//             )
+//         }
+        
+//     }
+// }
+
+// export default Plausible;
